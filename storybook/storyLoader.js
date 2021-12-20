4b9b6b11e9a2fa795d3653eabb11182f017160d0
YoDenzel <79210308+YoDenzel@users.noreply.github.com>
2021-12-20 14:33:44 +0200

    initital commit

---
+++ storybook/storyLoader.js
@@ -0,0 +1,81 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/shared/ui/core/atoms/typography/typography.story');
+  require('../src/shared/ui/core/molecules/accepted-declined-text/accepted-declined-text.story');
+  require('../src/shared/ui/core/molecules/cashback-text/cashback-text.story');
+  require('../src/shared/ui/core/molecules/confirmation-condition/confirmation-condition.story');
+  require('../src/shared/ui/core/molecules/confirmation-info/confirmation-info.story');
+  require('../src/shared/ui/core/molecules/credit-card-block/credit-card-block.story');
+  require('../src/shared/ui/core/molecules/custom-keyboard-single-number/custom-keyboard-single-number.story');
+  require('../src/shared/ui/core/molecules/go-back/go-back.story');
+  require('../src/shared/ui/core/molecules/header-for-main/header-for-main.story');
+  require('../src/shared/ui/core/molecules/header-for-money-transfer-page/header-for-money-transfer-page.story');
+  require('../src/shared/ui/core/molecules/kode-logo-molecule/kode-logo-molecule.story');
+  require('../src/shared/ui/core/molecules/menu-item/menu-item.story');
+  require('../src/shared/ui/core/molecules/money-chips/money-chips.story');
+  require('../src/shared/ui/core/molecules/money-sum-input/money-sum-input.story');
+  require('../src/shared/ui/core/molecules/phone-number-input/phone-number-input.story');
+  require('../src/shared/ui/core/molecules/searchbar/searchbar.story');
+  require('../src/shared/ui/core/molecules/snackbar/snackbar.story');
+  require('../src/shared/ui/core/molecules/submit-button/submit-button.story');
+  require('../src/shared/ui/core/molecules/tab-bar-icon/tab-bar-icon.story');
+  require('../src/shared/ui/core/organisms/confirmation-list/confirmation-list.story');
+  require('../src/shared/ui/core/organisms/custom-keyboard/custom-keyboard.story');
+  require('../src/shared/ui/core/organisms/header-for-services/header-for-services.story');
+  require('../src/shared/ui/core/organisms/menu-list/menu-list.story');
+  require('../src/shared/ui/core/organisms/menu-list/service-list.story');
+  require('../src/shared/ui/core/organisms/money-sum-block/money-sum-block.story');
+  require('../src/shared/ui/core/pages/accepted-declined-page/accepted-declined-page.story');
+  require('../src/shared/ui/core/pages/confirmation-page/confirmation-page.story');
+  require('../src/shared/ui/core/pages/money-transfer-page/money-transfer-page.story');
+  require('../src/shared/ui/core/pages/payment-page/payment-page.story');
+  require('../src/shared/ui/core/pages/phone-auth-page/phone-auth-page.story');
+  require('../src/shared/ui/core/pages/plug-page/plug-page.story');
+  require('../src/shared/ui/core/pages/service-page/service-page.story');
+  require('../src/shared/ui/core/typography/typography.story');
+}
+
+const stories = [
+  '../src/shared/ui/core/atoms/typography/typography.story',
+  '../src/shared/ui/core/molecules/accepted-declined-text/accepted-declined-text.story',
+  '../src/shared/ui/core/molecules/cashback-text/cashback-text.story',
+  '../src/shared/ui/core/molecules/confirmation-condition/confirmation-condition.story',
+  '../src/shared/ui/core/molecules/confirmation-info/confirmation-info.story',
+  '../src/shared/ui/core/molecules/credit-card-block/credit-card-block.story',
+  '../src/shared/ui/core/molecules/custom-keyboard-single-number/custom-keyboard-single-number.story',
+  '../src/shared/ui/core/molecules/go-back/go-back.story',
+  '../src/shared/ui/core/molecules/header-for-main/header-for-main.story',
+  '../src/shared/ui/core/molecules/header-for-money-transfer-page/header-for-money-transfer-page.story',
+  '../src/shared/ui/core/molecules/kode-logo-molecule/kode-logo-molecule.story',
+  '../src/shared/ui/core/molecules/menu-item/menu-item.story',
+  '../src/shared/ui/core/molecules/money-chips/money-chips.story',
+  '../src/shared/ui/core/molecules/money-sum-input/money-sum-input.story',
+  '../src/shared/ui/core/molecules/phone-number-input/phone-number-input.story',
+  '../src/shared/ui/core/molecules/searchbar/searchbar.story',
+  '../src/shared/ui/core/molecules/snackbar/snackbar.story',
+  '../src/shared/ui/core/molecules/submit-button/submit-button.story',
+  '../src/shared/ui/core/molecules/tab-bar-icon/tab-bar-icon.story',
+  '../src/shared/ui/core/organisms/confirmation-list/confirmation-list.story',
+  '../src/shared/ui/core/organisms/custom-keyboard/custom-keyboard.story',
+  '../src/shared/ui/core/organisms/header-for-services/header-for-services.story',
+  '../src/shared/ui/core/organisms/menu-list/menu-list.story',
+  '../src/shared/ui/core/organisms/menu-list/service-list.story',
+  '../src/shared/ui/core/organisms/money-sum-block/money-sum-block.story',
+  '../src/shared/ui/core/pages/accepted-declined-page/accepted-declined-page.story',
+  '../src/shared/ui/core/pages/confirmation-page/confirmation-page.story',
+  '../src/shared/ui/core/pages/money-transfer-page/money-transfer-page.story',
+  '../src/shared/ui/core/pages/payment-page/payment-page.story',
+  '../src/shared/ui/core/pages/phone-auth-page/phone-auth-page.story',
+  '../src/shared/ui/core/pages/plug-page/plug-page.story',
+  '../src/shared/ui/core/pages/service-page/service-page.story',
+  '../src/shared/ui/core/typography/typography.story',
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};

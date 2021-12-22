e26feae3c8601b2d7fcbccac519db6874fd01ab3
YoDenzel <kombogame6@gmail.com>
2021-12-22 02:19:05 +0200

    feat: navigation to app

--- src/pages/ui/connectors/everything-good-page-connector/everything-good-page-connector.tsx
+++ src/pages/ui/connectors/everything-good-page-connector/everything-good-page-connector.tsx
@@ -1,9 +1,14 @@
 import { EverythingGoodPage } from '@shared/ui/core/pages/everything-good-page';
+import { TRoute } from '../confirmation-page-connector/types';
 
-export const EverythingGoodPageConnector = () => {
+export const EverythingGoodPageConnector = ({ navigation }: TRoute) => {
+  const checkConditions = () => {
+    navigation.navigate('tabs', {});
+  };
+
   return (
     <EverythingGoodPage
-      submitButton={{ title: 'Войти', checkConditions: () => void 0 }}
+      submitButton={{ title: 'Войти', checkConditions: checkConditions }}
       everythingReadyBlock={{
         title: 'Все готово',
         info: 'Теперь вы можете использовать мобильное приложение Kode Bank',
